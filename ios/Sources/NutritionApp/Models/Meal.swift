d7433c885d7da3444c68f688397d94ab009c6d47
adrianmfuentes <amf13azul@proton.me>
2026-01-20 21:14:50 +0100

    feat: eliminar archivos y carpetas de iOS no utilizados, actualizar documentación y mejorar la estructura de la API

--- ios/Sources/NutritionApp/Models/Meal.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct Meal: Identifiable, Codable {
-    let id: String
-    let name: String
-    let date: String
-    let totalCalories: Double
-    let foods: [NutritionAnalysis.FoodItem]
-}
